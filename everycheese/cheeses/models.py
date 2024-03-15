723c7befc6af634f088d8f42bf24704bfe4fc682
SaiSakethGK <saisaketh.gootykase@unh.edu>
2024-03-15 04:38:34 -0400

    Add Cheese Rating functionality

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -3,6 +3,14 @@ from model_utils.models import TimeStampedModel
 from autoslug import AutoSlugField
 from django_countries.fields import CountryField
 from django.urls import reverse
+from django.db import models
+from model_utils.models import TimeStampedModel
+from autoslug import AutoSlugField
+from django_countries.fields import CountryField
+from django.urls import reverse
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models import Avg
 
 
 
@@ -19,15 +27,48 @@ class Cheese(TimeStampedModel):
         SEMI_SOFT = "semi-soft", "Semi-Soft"
         SEMI_HARD = "semi-hard", "Semi-Hard"
         HARD = "hard", "Hard"
-        # Other Fields Here...
+
+    firmness = models.CharField("Firmness", max_length=20,choices=Firmness.choices, default=Firmness.UNSPECIFIED)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL,null=True,on_delete=models.SET_NULL)
 
     
-    def get_absolute_url(self):
-        return reverse('cheeses:detail', kwargs={"slug": self.slug})
-    
-    firmness = models.CharField("Firmness", max_length=20, choices=Firmness.choices, default=Firmness.UNSPECIFIED)
+    @property
+    def average_rating(self):
+        # Best to calculate in the query.
+        # This is easier until we know more:
+        ratings = Rating.objects.all().filter(cheese=self)
+        
+        if not ratings:
+            return 0
+        
+        total = 0
+        count = 0
+
+        for r in ratings:
+            total += r.i_rating
+            count += 1
+
+        if count <= 0:
+            return 9
+        
+        return total // count
+
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        """Return absolute URL to the Cheese Detail page."""
+        return reverse('cheeses:detail', kwargs={"slug": self.slug})
+    
+
+class Rating(models.Model):
+
+    i_rating = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+    cheese = models.ForeignKey(Cheese, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.i_rating}"
 
 
--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -4,30 +4,28 @@ from .models import Cheese
 from django.urls import reverse_lazy
 import logging
 from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView
+from .models import Cheese, Rating
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+import logging  # Import the logging module
 
 log = logging.getLogger(__name__)
 
 class CheeseListView(ListView):
     model = Cheese
-    
-    def get_context_data(self, **kwargs: Any):
-        log.info("Hello World")
-        context = super().get_context_data(**kwargs)
-        context['my_new_ch_list']=Cheese.objects.all()
-        
-        return context
 
 class CheeseDetailView(DetailView):
     model = Cheese
 
-class CheeseCreateView(CreateView):
+class CheeseCreateView(LoginRequiredMixin, CreateView):
     model = Cheese
-    fields = [
-        'name',
-        'description',
-        'firmness',
-        'country_of_origin',
-    ]
+    fields = ['name', 'description', 'firmness',
+                'country_of_origin']
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
 
 class CheeseDeleteView(DeleteView):
     model = Cheese
@@ -41,12 +39,42 @@ class ConfirmCheeseDeleteView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['cheese'] = Cheese.objects.get(slug=self.kwargs['slug'])
         return context
-
-class CheeseUpdateView(UpdateView):
+    
+class CheeseUpdateView(LoginRequiredMixin, UpdateView):
     model = Cheese
-    fields = ['name', 'description', 'firmness', 'country_of_origin']
-    template_name = 'cheeses/cheese_update_form.html'
-    context_object_name = 'cheese'
+    fields = [
+    'name',
+    'description',
+    'firmness',
+    'country_of_origin'
+    ]
+    
+    action = "Update"
 
-    def get_success_url(self):
-        return reverse_lazy('cheeses:detail', kwargs={'slug': self.object.slug})
+    def get_context_data(self, **kwargs):
+        ctx = super(CheeseUpdateView, self).get_context_data(**kwargs)
+        _slug = self.kwargs.get("slug")
+        ch = Cheese.objects.all().filter(slug = _slug).first()
+
+        if ch ==None:
+            ctx["rating"] =0
+            return
+        r = Rating.objects.all().filter(creator = self.request.user, cheese = ch).first()
+
+        if r != None:
+            ctx["rating"] = r.i_rating
+        else:
+            ctx["rating"] = 0
+        return ctx
+    def form_valid(self, form):
+        # Get the cheese being updated
+        cheese = self.object
+
+        # Get or create the rating for the current user and cheese
+        rating, created = Rating.objects.get_or_create(creator=self.request.user, cheese=cheese)
+
+        # Update the rating value based on the form data
+        rating.i_rating = int(self.request.POST.get('rating'))  # Safely retrieve the rating value
+        rating.save()
+
+        return super().form_valid(form)
